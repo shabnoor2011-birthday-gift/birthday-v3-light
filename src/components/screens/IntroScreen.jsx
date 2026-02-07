7974cd26e8807cb2797364cc7d7c3cc3730b07bb
shabnoor2011-birthday-gift <samrins778@gmail.com>
2026-02-07 17:41:54 +0530

    Update IntroScreen.jsx

--- src/components/screens/IntroScreen.jsx
+++ src/components/screens/IntroScreen.jsx
@@ -23,7 +23,7 @@ export default function IntroScreen({ onNext }) {
                     style={{
                         filter: "drop-shadow(0 0 20px rgba(255,105,180,0.4))",
                     }}>
-                    A Cutiepie was born today, 21 years ago!
+                    A good boy was born today, 18 years ago!
                 </h1>
                 <p className="mt-4 text-foreground will-change-transform">Yes, it’s YOU! A little surprise awaits...</p>
             </div>
